f987c3ce1323802e6c8ebb1fde6fadd571db52d7
Dlouhan Frankie <petr1kaspar@gmail.com>
2018-01-07 15:45:37 +0100

    Little changes of port in server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ var router = express.Router();
 
 //set our port to either a predetermined port number if you have set 
 //it up, or 3001
-var port = process.env.API_PORT || 3001;
-
+//var port = process.env.API_PORT || 3001;
+var port = process.env.PORT;
 //db config
 var dbuser = 'petr2';
 var dbpassword = '123456';
@@ -180,13 +180,13 @@ router.route('/threads')
 
 //Use our router configuration when we call /api
 //app.use('/api', router);
-
 // Express only serves static assets in production
 //if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  //app.use(express.static('build'));
   //app.use('/build', router);
 //}
 
+app.use(express.static(path.join(__dirname, '/build')));
 //starts the server and listens for requests
 app.listen(port, function() {
     console.log(`api running on port ${port}`);
